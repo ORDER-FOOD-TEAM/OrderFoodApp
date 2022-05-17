db65c5db98afdd6887bc1c32d90581b2bdb2309d
PhamneyuT <koduapass@gmail.com>
2022-05-17 19:00:25 +0700

    add animation when switching between fragments

--- app/src/main/java/com/example/orderfoodapp/activities/MainMenuActivity.kt
+++ app/src/main/java/com/example/orderfoodapp/activities/MainMenuActivity.kt
@@ -1,8 +1,9 @@
 package com.example.orderfoodapp.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.orderfoodapp.R
 import com.example.orderfoodapp.fragments.FavouriteFagment
@@ -14,12 +15,18 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+//TO NOT WASTE TIME ON REFER ONE BY ONE ELEMENT
+import kotlinx.android.synthetic.main.activity_main_menu.*
+
 
 class MainMenuActivity : AppCompatActivity() {
 
     private val mainMenuFragment = MainMenuFragment()
     private val profileFragment = ProfileFragment()
-    private val favouriteFagment = FavouriteFagment()
+    private val favouriteFragment = FavouriteFagment()
+
+    //private val bottom_app_bar = findViewById<BottomNavigationView>(R.id.bottom_app_bar)
+    //private val shoppingCart_button = findViewById<FloatingActionButton>(R.id.shoppingCart_button)
 
     //initial the main menu fragment to be the first fragment
     private var curFragment: Fragment = mainMenuFragment
@@ -37,6 +44,62 @@ class MainMenuActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, mainMenuFragment)
         transaction.commit()
+
+        bottom_app_bar.setOnNavigationItemSelectedListener {
+            //to hide shoppingCart button at the profile fragment
+            when(it.itemId) {
+                R.id.navbottombar_profile -> {
+                    replaceFragment(profileFragment)
+                    shoppingCart_button.visibility = View.GONE
+                }
+                R.id.navbottombar_home -> {
+                    replaceFragment(mainMenuFragment)
+                    shoppingCart_button.visibility = View.VISIBLE
+                }
+                R.id.navbottombar_favourite -> {
+                    replaceFragment(favouriteFragment)
+                    shoppingCart_button.visibility = View.VISIBLE
+                }
+            }
+            true
+        }
+
+        //move to Cart and start cart activity
+        shoppingCart_button.setOnClickListener() {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    //to switch between tabs from the bottom navigation bar
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+
+        if(curFragment == mainMenuFragment) {
+            transaction.setCustomAnimations(
+                R.anim.enter_right_to_left, R.anim.exit_right_to_left)
+        }
+        else if(curFragment == profileFragment) {
+            transaction.setCustomAnimations(
+                R.anim.enter_left_to_right, R.anim.exit_left_to_right)
+        }
+        else if(curFragment == favouriteFragment) {
+            if(fragment == mainMenuFragment)
+                transaction.setCustomAnimations(
+                    R.anim.enter_left_to_right, R.anim.exit_left_to_right)
+            else
+                transaction.setCustomAnimations(
+                    R.anim.enter_right_to_left, R.anim.exit_right_to_left)
+        }
+
+        transaction.hide(curFragment)
+        if(fragment.isAdded)
+            transaction.show(fragment)
+        else
+            transaction.add(R.id.fragment_container,fragment)
+
+        transaction.commit()
+        curFragment = fragment
     }
 
     private fun checkFullFillInformation() {
