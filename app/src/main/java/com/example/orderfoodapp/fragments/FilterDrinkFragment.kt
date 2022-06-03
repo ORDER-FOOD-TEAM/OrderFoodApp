56dcb87d7d7d1637feaf5cc8bae93074120e55ff
PhamneyuT <koduapass@gmail.com>
2022-06-03 16:34:57 +0700

    filter drink

--- app/src/main/java/com/example/orderfoodapp/fragments/FilterDrinkFragment.kt
+++ app/src/main/java/com/example/orderfoodapp/fragments/FilterDrinkFragment.kt
@@ -5,28 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.orderfoodapp.R
+import com.example.orderfoodapp.adapters.DishAdapter
+import com.example.orderfoodapp.models.Dish
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_drink.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FilterDrinkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FilterDrinkFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var dishAdapterDrink: DishAdapter
+
+//    private lateinit var database: FirebaseDatabase
+//    private lateinit var ref: DatabaseReference
+
+    private lateinit var map: HashMap<String, String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        map = HashMap()
+        val bundle = this.arguments
+        if(bundle != null) {
+            map = bundle.getSerializable("map") as HashMap<String, String>
         }
     }
 
@@ -38,23 +42,76 @@ class FilterDrinkFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_drink, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FillterDrinkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FilterDrinkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onResume() {
+        super.onResume()
+
+        //change the adapter of recyclerView to the custom adapter
+        dishAdapterDrink = DishAdapter(mutableListOf())
+        drink_recyclerView.adapter = dishAdapterDrink
+
+        val layoutManager = GridLayoutManager(context,2)
+        drink_recyclerView.layoutManager = layoutManager
+
+        val dbRef = FirebaseDatabase.getInstance().getReference("Product")
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //to delete the local
+                dishAdapterDrink.deleteAll()
+                //then update the new list
+                for(data in snapshot.children) {
+                    if((data.child("category").value as String) == "Drinking") {
+                        val prName = data.child("provider").value as String
+                        if(map.containsKey(prName)) {
+                            val deliveryTime = map[prName]
+
+                            var priceS = 0.0
+                            val a: Any = data.child("priceS").value as Any
+                            val typeA = a::class.simpleName
+                            if(typeA == "Long" || typeA == "Double")
+                                priceS = a.toString().toDouble()
+
+                            var priceM = 0.0
+                            val b: Any = data.child("priceM").value as Any
+                            val typeB = b::class.simpleName
+                            if(typeB == "Long" || typeB == "Double")
+                                priceM = b.toString().toDouble()
+
+                            var priceL = 0.0
+                            val c: Any = data.child("priceL").value as Any
+                            val typeC = c::class.simpleName
+                            if(typeC == "Long" || typeC == "Double")
+                                priceL = c.toString().toDouble()
+
+                            val dish = Dish(
+                                data.child("id").value as String,
+                                data.child("name").value as String,
+                                priceS,
+                                priceM,
+                                priceL,
+                                data.child("rated").value as String,
+                                deliveryTime!!,
+                                data.child("category").value as String,
+                                data.child("description").value as String,
+                                data.child("salePercent").value as Long,
+                                data.child("provider").value as String,
+                                data.child("amountS").value as Long,
+                                data.child("amountSsold").value as Long,
+                                data.child("amountM").value as Long,
+                                data.child("amountMsold").value as Long,
+                                data.child("amountL").value as Long,
+                                data.child("amountLsold").value as Long,
+                            )
+                            dishAdapterDrink.addDish(dish)
+                        }
+                    }
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+                //null
+            }
+        })
+
     }
+
 }
