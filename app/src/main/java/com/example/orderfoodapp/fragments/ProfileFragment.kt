bc78ed76422608d936d8a4947da75963c899a9a8
vfa-locltb <locltb@vitalify.asia>
2022-06-10 22:15:53 +0700

    Fix color

--- app/src/main/java/com/example/orderfoodapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/orderfoodapp/fragments/ProfileFragment.kt
@@ -14,6 +14,7 @@ import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.example.orderfoodapp.R
 import com.example.orderfoodapp.activities.*
+import com.example.orderfoodapp.models.OrderHistory
 import com.facebook.login.LoginManager
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.firebase.auth.FirebaseAuth
